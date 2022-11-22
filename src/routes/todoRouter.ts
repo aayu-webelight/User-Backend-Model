481ec66ba120e3a952219d36e5eb8fe2fff0aaf5
Ansh Shah <85743845+Anshshah-1609@users.noreply.github.com>
2022-11-22 19:09:46 +0530

    Update and rename todo.ts to todoRouter.ts

--- src/routes/todoRouter.ts
+++ src/routes/todoRouter.ts
@@ -2,10 +2,10 @@ import { auth } from "middleware/auth";
 import { addTodo, findAllTodo } from "controllers/TodoController";
 import express from "express";
 
-export const todoRouter = express.Router();
+const todoRouter = express.Router();
 
 todoRouter.get("/todo", auth, findAllTodo);
 
 todoRouter.post("/todo", auth, addTodo);
 
-// export { router as todoRouter };
+export default todoRouter;
